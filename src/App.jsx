c3918b8df9bcc7f2ad7c7c86da128da783e32387
Rony2518 <113433588+Rony2518@users.noreply.github.com>
2023-12-24 13:28:04 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Intro from '../components/Intro'
+import Details from '../components/Details'
+import Contact from '../components/Contact'
+import './App.css'
+
+function App() {
+  return (
+    <div className='main_box'>
+      <Intro />
+      <Details />
+      <Contact />
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './App.css'
+
+const node = document.getElementById("root")
+ReactDOM.createRoot(node).render(
+  <App />
+)
+
